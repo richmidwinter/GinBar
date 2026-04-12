fe2aba37e04c8fb9a038db6f9ded0d39c96ed1fa
Rich Midwinter <rich.midwinter@gmail.com>
2026-04-12 20:22:56 +0100

    Initial Commit

---
+++ GinBar/GinBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  GinBarApp.swift
+//  GinBar
+//
+//  Created by rich on 12/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct GinBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
